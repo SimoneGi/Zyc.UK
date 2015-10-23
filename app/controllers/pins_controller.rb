821386e12347f7f194f6c25d832c50433807bf45
Simone <Simone.v.gill@wpfentongroup.co.uk>
2015-10-23 12:48:29 +0100

    fun

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -61,7 +61,7 @@ class PinsController < ApplicationController
 
     
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
 
